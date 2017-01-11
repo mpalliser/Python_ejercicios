99438d4b6eabe07f78ba1bbf557a9b46c42f24d2
Mariano <mariano.palliser@gmail.com>
2017-01-11 20:07:16 +0100

    refactorizado chance

--- Poo/Yatzy_Refactoring_TDD_Kata/yatzy.py
+++ Poo/Yatzy_Refactoring_TDD_Kata/yatzy.py
@@ -1,13 +1,10 @@
 class Yatzy:
 
     @staticmethod
-    def chance(d1, d2, d3, d4, d5):
+    def chance(*dice):
         total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
+        for die in dice:
+            total = total + die
         return total
 
     @staticmethod
