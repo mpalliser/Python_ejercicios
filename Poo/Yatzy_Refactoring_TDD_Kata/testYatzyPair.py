72a5131f08e34ffcbd127fd3bb538cc08a8f9289
Mariano <mariano.palliser@gmail.com>
2017-01-15 16:39:21 +0100

    Add files via upload

--- Poo/Yatzy_Refactoring_TDD_Kata/testYatzyPair.py
+++ Poo/Yatzy_Refactoring_TDD_Kata/testYatzyPair.py
@@ -62,6 +62,61 @@ def test_two_pair():
     #player scores the sum of these dice. 
     assert 8 == Yatzy.two_pair(1,1,2,3,3)
     assert 0 == Yatzy.two_pair(1,1,2,3,4)
+    assert 0 == Yatzy.two_pair(1,4,2,5,3)
     assert 6 == Yatzy.two_pair(1,1,2,2,2)
+    assert 0 == Yatzy.two_pair(1,3,3,3,3)
+
+def test_three_of_a_kind():
+    '''
+    If there are three dice with the same number, the player
+    scores the sum of these dice. 
+    '''
+    assert 9 == Yatzy.three_of_a_kind(5,4,3,3,3)
+    assert 0 == Yatzy.three_of_a_kind(1,1,2,3,3)
+    assert 12 == Yatzy.three_of_a_kind(4,4,4,4,1)
+
+def test_four_of_a_kind():
+    '''
+    If there are four dice with the same number, the player
+    scores the sum of these dice.  
+    '''
+    assert 24 == Yatzy.four_of_a_kind(6,6,6,6,1)
+    assert 0 == Yatzy.four_of_a_kind(2,2,2,1,1)
+    assert 4 == Yatzy.four_of_a_kind(3,1,1,1,1)
+    assert 12 == Yatzy.four_of_a_kind(3,3,3,3,3)
+
   
+
+def test_small_straight(*dice):
+    '''
+    When placed on "small straight", if the dice read
+    1,2,3,4,5, the player scores 15 (the sum of all the dice).
+    '''
+    assert 15 == Yatzy.smallStraight(1,2,3,4,5)
+    assert 0 == Yatzy.smallStraight(2,3,4,5,6)
+    assert 0 == Yatzy.smallStraight(1,2,2,3,4)
+    assert 0 == Yatzy.smallStraight(3,4,5,6,1)
+
+def test_large_straight(*dice):
+    '''
+    When placed on "large straight", if the dice read2,3,4,5,6,
+    the player scores 20 (the sum of all the dice).
+    '''
+    assert 20 == Yatzy.largeStraight(2,3,4,5,6)
+    assert 20 == Yatzy.largeStraight(3,4,5,6,2,1)
+    assert 0 == Yatzy.largeStraight(2,2,3,4,5)
+    assert 0 == Yatzy.largeStraight(1,2,3,4,5)
+
+def test_full_house(*dice):
+    '''
+    If the dice are two of a kind and three of a kind, the player scores the sum of all the dice.
+    '''
+    assert 8 == Yatzy.fullHouse(1,1,2,2,2)
+    assert 0 == Yatzy.fullHouse(2,2,3,3,4)
+    assert 0 == Yatzy.fullHouse(4,4,4,4,4)
+    assert 0 == Yatzy.fullHouse(1,1,2,3,4)
+    assert 0 == Yatzy.fullHouse(1,2,3,4,6)
+    assert 0 == Yatzy.fullHouse(1,1,3,5,2)
+    assert 0 == Yatzy.fullHouse(1,5,2,2,2)
+
 
